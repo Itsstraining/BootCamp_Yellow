e07610e9cbf73c506da5990ff3e5f5bd3e7f8253
art3mis145 <art3mis145@gmail.com>
2021-02-03 17:57:33 +0700

    duy

--- Fontend/src/app/pages/Duy/room/room.component.ts
+++ Fontend/src/app/pages/Duy/room/room.component.ts
@@ -3,13 +3,32 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-room',
   templateUrl: './room.component.html',
-  styleUrls: ['./room.component.scss']
+  styleUrls: ['./room.component.scss'],
 })
 export class RoomComponent implements OnInit {
+  public players = [
+    'P1',
+    'P2',
+    'P1',
+    'P2',
+    'P1',
+    'P2',
+    'P1',
+    'P2',
+    'P1',
+    'P2',
+    'P1',
+    'P2',
+    'P1',
+    'P2',
+    'P1',
+    'P2',
+    'P1',
+    'P2',
+    'P1',
+    'P2',
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
