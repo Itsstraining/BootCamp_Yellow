c307e3186e7459b9da410f82cc054f20d40e9e0b
Long minh <longminh207@gmail.com>
2021-02-04 11:47:17 +0700

    long

--- Fontend/src/app/pages/Long/rank-view/rank-view.module.ts
+++ Fontend/src/app/pages/Long/rank-view/rank-view.module.ts
@@ -3,13 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { RankViewRoutingModule } from './rank-view-routing.module';
 import { RankViewComponent } from './rank-view.component';
-
+import { MatTableModule } from '@angular/material/table';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 
 @NgModule({
   declarations: [RankViewComponent],
   imports: [
     CommonModule,
-    RankViewRoutingModule
+    RankViewRoutingModule,
+    MatTableModule,
+    MatButtonModule,
+    MatIconModule,
+    MatProgressBarModule,
   ]
 })
 export class RankViewModule { }
