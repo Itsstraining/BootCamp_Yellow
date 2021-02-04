6900849310533f98e09396bff2c9aa517bb7d458
art3mis145 <art3mis145@gmail.com>
2021-02-04 13:00:01 +0700

    duy

--- Fontend/src/app/pages/Duy/login/login.component.ts
+++ Fontend/src/app/pages/Duy/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { Router, RouterLink } from '@angular/router';
 import * as firebase from 'firebase';
 
 @Component({
@@ -8,16 +9,24 @@ import * as firebase from 'firebase';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  constructor(public auth: AngularFireAuth) {}
+  constructor(public auth: AngularFireAuth, public router: Router) {}
+  public user = null;
   async login() {
     const provider = new firebase.default.auth.GoogleAuthProvider();
     try {
       await this.auth.signInWithPopup(provider);
+      this.router.navigate(['menuhome']);
       alert('logged in');
     } catch (err) {
       alert(err);
     }
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.auth.authState.subscribe((auth) => {
+      if (this.user == null) {
+        this.user = auth;
+      }
+    });
+  }
 }
